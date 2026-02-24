374b8f0af85c56018348dcef185527fd16ed2372
Pavlo Vovk <dazkok@gmail.com>
2026-02-24 19:36:13 +0100

    tasks CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->to('/tasks');
 });
+
+Route::resource('tasks', TaskController::class);
+Route::put('tasks/{task}/reorder', [TaskController::class, 'reorder'])->name('tasks.reorder');
